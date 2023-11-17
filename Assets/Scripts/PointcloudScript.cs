abe7e50f89c620778940c3537214c444ede3d9c2
Markus Rychter <89453088+Marryc02@users.noreply.github.com>
2023-11-17 18:13:09 +0100

    Did a bunch of stuff. No idea if it worked or not
    Have to fix an out of bounds thingy on the smoothTerrain function

--- Assets/Scripts/PointcloudScript.cs
+++ Assets/Scripts/PointcloudScript.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Globalization;
 using System;
 using UnityEngine;
+using System.Xml.Schema;
+//using System.Numerics;
+//using System.Numerics;
 
 
 // I borrowed a lot of code from Joachim in the class. Ordinarily I would not borrow this much code from someone,
@@ -22,6 +25,7 @@ public class PointcloudScript : MonoBehaviour
 
     string mergedText = @"Assets/Resources/merged.txt";
     string terrainText = @"Assets/Resources/terrain.txt";
+    string smoothTerrain = @"Assets/Resources/smoothTerrain.txt";
 
     // Used for deciding how many lines should be skipped when converting the initial document and making a smooth pointcloud.
     float lineSkips = 25;
@@ -31,8 +35,9 @@ public class PointcloudScript : MonoBehaviour
     float yMin = 0; float yMax = 0;
     float zMin = 0; float zMax = 0;
 
+    float xStep = 10; float zStep = 7;
     float deltaX = 0; float deltaZ = 0;
-    float xStep = 0; float zStep = 0;
+    
 
 
     // Runs before Start().
@@ -49,17 +54,17 @@ public class PointcloudScript : MonoBehaviour
             // Finds min and max values.
             findMinAndMax(lineCount);
 
+
+
             // As it currently stands there are over a million lines in the text document, 
             // where virtually all of the lines have incredibly large values, and as such we will be scaling down the values to bring them closer
             // to the origin of the scene, thus making it easer for us to showcase them.
             ConvertMerged(lineCount);
             Debug.Log("Merged converted and reduced.");
 
-
             // -1 since we ignore the first line.
             lineCount = File.ReadLines(terrainText).Count() - 1;
             Debug.Log("Amount of lines in the new terrainText-file: " + lineCount);
-
             // Stores the entire terrain -file in a string that inherits its size from "lineCount".
             string[] lines = new string[lineCount];
             lines = File.ReadAllLines(terrainText);
@@ -67,6 +72,28 @@ public class PointcloudScript : MonoBehaviour
             lines[0] = lineCount.ToString();
             // Writes the entire lines -string into the terrainText file, without touching the very first line that tells the number of lines.
             File.WriteAllLines(terrainText, lines);
+
+
+
+            lineCount = File.ReadLines(terrainText).Count();
+            Debug.Log("Amount of lines in the terrain-file: " + lineCount);
+
+            // Here we convert our terrain -file to one that looks smoother, and that can help us achieve a smooth texture for our triangles later.
+            ConvertTerrainToSmooth(lineCount);
+            Debug.Log("smoothTerrain converted and reduced.");
+
+            // -1 since we ignore the first line.
+            // Checking this again just in case
+            lineCount = File.ReadLines(smoothTerrain).Count() - 1;
+            Debug.Log("Amount of lines in the new smoothTerrain-file: " + lineCount);
+
+            // Stores the entire smoothTerrain -file in a string that inherits its size from "lineCount".
+            lines = new string[lineCount];
+            lines = File.ReadAllLines(smoothTerrain);
+            // Overwrites the very first, empty line in the smoothTerrain -file.
+            lines[0] = lineCount.ToString();
+            // Writes the entire lines -string into the terrainText file, without touching the very first line that tells the number of lines.
+            File.WriteAllLines(smoothTerrain, lines);
         }
     }
 
@@ -236,6 +263,72 @@ public class PointcloudScript : MonoBehaviour
             else
             {
                 a++;
+            }
+        }
+    }
+
+    void ConvertTerrainToSmooth(int fileLength)
+    {
+        List<float> o = new List<float>();
+        List<float> p = new List<float>();
+
+        List<List<Vector3>> buckets = new List<List<Vector3>>();
+
+        deltaX = (xMax - xMin) / xStep;
+        deltaZ = (zMax - zMin) / zStep;
+        
+        float averageHeight = 0;
+
+        string line;
+
+        // Pass the file path and file name to the StreamReader constructor
+        StreamReader readFile = new StreamReader(terrainText);
+
+
+        for (int i = 1; i < fileLength; i++)
+        {
+            // Read the first line of text
+            line = readFile.ReadLine();
+
+            // Makes a new list of strings with the name "pointValues".
+            // Assigns the mergedText .txt-document as the value of the List, however it also splits each line in the .txt-document
+            // in such a way that the document writes a new line with everything that comes after a space in the .txt-document all while
+            // deleting empty spaces in the .txt-document.
+            // Lastly it converts the document to a List as it is technically just a really long string with a format.
+            List<String> pointValues = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList<string>();
+                
+            // Creates a new list like previously, but made of floats instead. 
+            // This is so that we can directly alter the individual values in the points.
+            // We also swap the y- and z- values as 'kartverket' uses the z-value to denote height which is a no-go for us.
+            // Lastly we subtract a high value that exists on every single line on the width- and length- axis so that we can bring the points
+            // closer to the scenes' origin.
+            List<float> fPointValues = new List<float>();
+            fPointValues.Add(float.Parse(pointValues[0], CultureInfo.InvariantCulture.NumberFormat));
+            fPointValues.Add(float.Parse(pointValues[2], CultureInfo.InvariantCulture.NumberFormat));
+            fPointValues.Add(float.Parse(pointValues[1], CultureInfo.InvariantCulture.NumberFormat));
+
+            o[i - 1] = (fPointValues[0] - xMin) / deltaX;
+            p[i - 1] = (fPointValues[2] - zMin) / deltaZ;   
+
+            buckets[(int)Math.Round(o[i - 1])][(int)Math.Round(p[i - 1])] = (new Vector3(fPointValues[0], fPointValues[1], fPointValues[2]));
+
+            // Clears the list to save memory.
+            fPointValues.Clear();
+        }
+
+        for (int i = 0; i < buckets[0].Count; i++)
+        {
+            for (int j = 0; j < buckets[1].Count; j++)
+            {
+                averageHeight += buckets[1][j].y;
+            }
+            averageHeight = averageHeight / buckets[1].Count;
+
+            string outputString = (xMin + (deltaX / 2) + (deltaX * o[i])) + " " + averageHeight + " " + (zMin + (deltaZ / 2) + (deltaZ * p[i]));
+
+            using (StreamWriter writeFile = File.AppendText(smoothTerrain))
+            {
+                writeFile.WriteLine(outputString);
             }
         }
     }
